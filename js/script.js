69ae2ef406ea6bc28f64bd01e02867008688038a
murkymao <glaciefer.1@gmail.com>
2017-12-14 17:56:45 +0200

    add slider

---
+++ js/script.js
@@ -0,0 +1,14 @@
+var btn_prev = document.querySelector('#gallery .buttons .prev' );
+var btn_next = document.querySelector('#gallery .buttons .next' );
+var images = document.querySelectorAll('#gallery .photos img' );
+var i = 0;
+
+btn_prev.onclick = function () {
+    alert(1);
+};
+
+btn_next.onclick = function () {
+    images[i].style.display = 'none';
+    i++;
+    images[i].style.display = 'block';
+};
