56cfe940a2930de6c4b7ae69c9972a1d3345d4f6
murkymao <glaciefer.1@gmail.com>
2017-12-14 18:17:11 +0200

    fix slider

--- js/script.js
+++ js/script.js
@@ -4,11 +4,21 @@ var images = document.querySelectorAll('#gallery .photos img' );
 var i = 0;
 
 btn_prev.onclick = function () {
-    alert(1);
+    images[i].style.display = 'none';
+    i--;
+
+    if(i < 0){
+        i = images.length - 1;
+    }
+    images[i].style.display = 'block';
 };
 
 btn_next.onclick = function () {
     images[i].style.display = 'none';
     i++;
+
+    if(i >= images.length){
+        i = 0;
+    }
     images[i].style.display = 'block';
 };
